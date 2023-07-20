30ee26a53baa4a9ae9cc457e2a0603df8398703b
Leon <leon.yee000@gmail.com>
2023-07-20 13:24:48 -0700

    working recommender

--- examples/movie-recommender/main.py
+++ examples/movie-recommender/main.py
@@ -1,135 +1,64 @@
-# https://grouplens.org/datasets/movielens/latest/
-
 import lancedb
 
-import pandas as pd
 import numpy as np
-import tensorflow as tf
-import keras
-from keras import layers
-import model
-
-# load user review dataset, get movie models, create embeddings, create table with data, query, return results
-
-ratings = pd.read_csv('./ml-latest-small/ratings.csv')
-ratings = ratings.dropna()
-
-userIDs = ratings['userId'].unique().tolist()
-user_encoded = {x: i for i, x in enumerate(userIDs)}
-
-movieIDs = ratings['movieId'].unique().tolist()
-movie_encoded = {x: i for i, x in enumerate(movieIDs)}
-movie_encoded_inv = {i: x for i, x in enumerate(movieIDs)}
-
-num_users = len(user_encoded)
-num_movies = len(movie_encoded_inv)
-
-ratings["rating"] = ratings["rating"].values.astype(np.float32)
-min_rating = min(ratings["rating"])
-max_rating = max(ratings["rating"])
-
-print(">> num_users:", num_users, "num_movies:", num_movies, "min_rating:", min_rating, "max_rating:", max_rating)
+import pandas as pd
 
 
-ratings = ratings.sample(frac=1, random_state=np.random.RandomState())
+def get_recommendations(title):
+    pd_data = pd.DataFrame(data)
+    # Table Search
+    result = table.search(pd_data[pd_data['title'] == title]["vector"].values[0]).limit(5).to_df()
+
+    # Get IMDB links
+    links = pd.read_csv('./ml-latest-small/links.csv', header=0, names=["movie id", "imdb id", "tmdb id"], converters={'imdb id': str})
+
+    ret = result['title'].values.tolist()
+    # Loop to add links
+    for i in range(len(ret)):
+        link = links[links['movie id'] == result['id'].values[i]]["imdb id"].values[0]
+        link = "https://www.imdb.com/title/tt" + link
+        ret[i] = [ret[i], link]
+    return ret
 
 
-# split data into train and test sets
+if __name__ == "__main__":
 
-x = ratings[["userId", "movieId"]].values
-y = ratings["rating"].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
+    # Load and prepare data
+    ratings = pd.read_csv('./ml-latest-small/ratings.csv', header=None, names=["user id", "movie id", "rating", "timestamp"])
+    ratings = ratings.drop(columns=['timestamp'])
+    ratings = ratings.drop(0)
+    ratings["rating"] = ratings["rating"].values.astype(np.float32)
+    ratings["user id"] = ratings["user id"].values.astype(np.int32)
+    ratings["movie id"] = ratings["movie id"].values.astype(np.int32)
 
-train_indices = int(0.9 * ratings.shape[0])
-x_train, x_test, y_train, y_test = (
-    x[:train_indices],
-    x[train_indices:],
-    y[:train_indices],
-    y[train_indices:],
-)
+    reviewmatrix = ratings.pivot(index="user id", columns="movie id", values="rating").fillna(0)
 
-print(">> x_train:", x_train.shape, "x_test:", x_test.shape, "y_train:", y_train.shape, "y_test:", y_test.shape)
-try:
-    model = keras.models.load_model('./trained_model1')
-except:
-    model = model.RecommenderNet(num_users + 1, num_movies + 1, 32)
-    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate=0.001))
+    # SVD
+    matrix = reviewmatrix.values
+    u, s, vh = np.linalg.svd(matrix, full_matrices=False)
 
-    history = model.fit(
-        x=x_train,
-        y=y_train,
-        batch_size=64,
-        epochs=5,
-        verbose=1,
-        validation_data=(x_test, y_test)
-    )
-
-    model.save('./trained_model1')
-
-print(model.summary())
+    vectors = np.rot90(np.fliplr(vh))
+    print(vectors.shape)
 
 
-test_loss = model.evaluate(x_test, y_test)
-print('\nTest Loss: {}'.format(test_loss))
-print("Testing Model with 1 user")
-movie_df = pd.read_csv("./ml-latest-small/movies.csv")
-user_id = "new_user"
-movies_watched_by_user = ratings.sample(5)
-movies_not_watched = movie_df[
-    ~movie_df["movieId"].isin(movies_watched_by_user.movieId.values)
-]["movieId"]
-movies_not_watched = list(
-    set(movies_not_watched).intersection(set(movie_encoded.keys()))
-)
-movies_not_watched = [[movie_encoded.get(x)] for x in movies_not_watched]
-user_movie_array = np.hstack(
-    ([[0]] * len(movies_not_watched), movies_not_watched)
-)
-print(pd.DataFrame(user_movie_array).head())
-ratings = model.predict(user_movie_array).flatten()
-top_ratings_indices = ratings.argsort()[-10:][::-1]
-recommended_movie_ids = [
-    movie_encoded_inv.get(movies_not_watched[x][0]) for x in top_ratings_indices
-]
-print("Showing recommendations for user: {}".format(user_id))
-print("====" * 9)
-print("Movies with high ratings from user")
-print("----" * 8)
-top_movies_user = (
-    movies_watched_by_user.sort_values(by="rating", ascending=False)
-    .head(5)
-    .movieId.values
-)
-movie_df_rows = movie_df[movie_df["movieId"].isin(top_movies_user)]
-for row in movie_df_rows.itertuples():
-    print(row.title, ":", row.genres)
-print("----" * 8)
-print("Top 10 movie recommendations")
-print("----" * 8)
-recommended_movies = movie_df[movie_df["movieId"].isin(recommended_movie_ids)]
-for row in recommended_movies.itertuples():
-    print(row.title, ":", row.genres)
+    # Metadata
+    movies = pd.read_csv('./ml-latest-small/movies.csv', header=0, names=["movie id", "title", "genres"])
+    movies = movies[movies['movie id'].isin(reviewmatrix.columns)]
+
+    data = []
+    for i in range(len(movies)):
+        data.append({"id": movies.iloc[i]["movie id"], "title": movies.iloc[i]['title'], "vector": vectors[i], "genre": movies.iloc[i]['genres']})
+    print(pd.DataFrame(data))
 
 
-"""
-li = []
-for filename in ['./ml-latest-small/links.csv', './ml-latest-small/movies.csv', './ml-latest-small/ratings.csv']:
-    df = pd.read_csv(filename, index_col=None, header=0)
-    li.append(df)
+    # Connect to LanceDB
+
+    db = lancedb.connect("./data/test-db")
+    try:
+        table = db.create_table("movie_set", data=data)
+    except:
+        table = db.open_table("movie_set")
 
 
-movies = pd.concat(li, axis=0, ignore_index=True)
-movies = movies.groupby(movies['movieId']).aggregate({'title': 'first', 'imdbId': 'first', 'tmdbId': 'first', 'genres': 'first', 'userId': 'first', 'rating': 'first'})
-movies = movies.drop_duplicates(subset=["imdbId"])
-print(movies)
-
-batch = 64
-for i in range(0, len(movies), batch):
-    end = min(i + batch, len(movies))
-    batch = movies.iloc[i:end]
-    embedding = model.predict(batch['title']).tolist()
-    metadata = batch.to_dict(orient="records")
-    ids = batch['imdbId'].values.tolist()
-
-    vector = list(zip(ids, embedding, metadata))
-    print(vector)
-"""
+    print(get_recommendations("Moana (2016)"))
+    print(get_recommendations("Rogue One: A Star Wars Story (2016)"))
