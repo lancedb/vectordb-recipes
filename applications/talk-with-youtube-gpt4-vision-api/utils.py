ba26539f2888e972a71ca44118b5d61461999c42
Prashant Dixit <54981696+PrashantDixit0@users.noreply.github.com>
2023-11-16 12:34:02 +0530

    Update utils.py

--- applications/talk-with-youtube-gpt4-vision-api/utils.py
+++ applications/talk-with-youtube-gpt4-vision-api/utils.py
@@ -19,23 +19,23 @@ def base64_converter(video_path):
 
 
 def prompting(base64Frames, openai_key):
-    if len(base64Frames)< 10:
-        base64Frames = base64Frames
-    elif len(base64Frames) < 100:
-        base64Frames = base64Frames[0::10]
-    elif len(base64Frames) < 1000:
-        base64Frames = base64Frames[0::100]
-    elif len(base64Frames) < 10000:
-        base64Frames = base64Frames[0::1000]
-    else:
-        st.error('Video is too large to handle by GPT4 Vision API', icon="🚨")
+    # if len(base64Frames)< 10:
+    #     base64Frames = base64Frames
+    # elif len(base64Frames) < 100:
+    #     base64Frames = base64Frames[0::10]
+    # elif len(base64Frames) < 1000:
+    #     base64Frames = base64Frames[0::100]
+    # elif len(base64Frames) < 10000:
+    #     base64Frames = base64Frames[0::1000]
+    # else:
+    #     st.error('Video is too large to handle by GPT4 Vision API', icon="🚨")
 
     PROMPT_MESSAGES = [
     {
         "role": "user",
         "content": [
             "These are frames from a video that I want to upload. Generate a description so that I can get all the information about video to chat with it.",
-            *map(lambda x: {"image": x, "resize": 360}, base64Frames),],},]
+            *map(lambda x: {"image": x, "resize": 360}, base64Frames[::50]),],},]
     
     params = {
         "model": "gpt-4-vision-preview",
