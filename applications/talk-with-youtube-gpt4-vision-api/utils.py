f65f84450c6f5d8a2bf85b4c1283f8ff05710beb
Ayush <ayush@chaurarsia@gmail.com>
2023-11-16 14:03:01 +0530

    hotfix: Support longer videos

--- applications/talk-with-youtube-gpt4-vision-api/utils.py
+++ applications/talk-with-youtube-gpt4-vision-api/utils.py
@@ -19,30 +19,28 @@ def base64_converter(video_path):
 
 
 def prompting(base64Frames, openai_key):
-    # if len(base64Frames)< 10:
-    #     base64Frames = base64Frames
-    # elif len(base64Frames) < 100:
-    #     base64Frames = base64Frames[0::10]
-    # elif len(base64Frames) < 1000:
-    #     base64Frames = base64Frames[0::100]
-    # elif len(base64Frames) < 10000:
-    #     base64Frames = base64Frames[0::1000]
-    # else:
-    #     st.error('Video is too large to handle by GPT4 Vision API', icon="🚨")
-
+    if len(base64Frames)< 5000:
+        base64Frames = base64Frames[::50]
+    elif len(base64Frames) < 10000:
+        base64Frames = base64Frames[::100]
+    elif len(base64Frames) < 100000:
+        base64Frames = base64Frames[::10000]
+    else:
+        st.error('Video is too large to handle by GPT4 Vision API', icon="🚨")
+    base64Frames = base64Frames[:50] if len(base64Frames) > 50 else base64Frames 
     PROMPT_MESSAGES = [
     {
         "role": "user",
         "content": [
             "These are frames from a video that I want to upload. Generate a description so that I can get all the information about video to chat with it.",
-            *map(lambda x: {"image": x, "resize": 360}, base64Frames[::50]),],},]
-    
+            *map(lambda x: {"image": x, "resize": 756}, base64Frames),],},]
+
     params = {
         "model": "gpt-4-vision-preview",
         "messages": PROMPT_MESSAGES,
         "api_key": openai_key,
         "headers": {"Openai-Version": "2020-11-07"},
-        "max_tokens": 200,
+        "max_tokens": 300,
     }
 
     result = openai.ChatCompletion.create(**params)
