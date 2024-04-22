007f8c210059930c619a534a98831c2bda3d1a4b
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2024-04-22 21:25:08 +0530

    Create constants.py

---
+++ applications/Chatbot_with_Parler_TTS/constants.py
@@ -0,0 +1,5 @@
+input_pdf = "https://d18rn0p25nwr6d.cloudfront.net/CIK-0001559720/8a9ebed0-815a-469a-87eb-1767d21d8cec.pdf"
+
+parler_tts_description = ''' Utilize a male voice with  an Indian English 
+accent for the chatbot. The speech should be clear, ensuring each word is 
+distinctly articulated in a crisp and confined audio environment. '''
