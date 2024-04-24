e768d084b6055aef757f88b050922e747c982734
Prashant Dixit <54981696+PrashantDixit0@users.noreply.github.com>
2024-04-24 18:59:28 +0530

    CLI based SDK manual chatbot with Phidata (#175)
    * assests and app name
    
    * update README
    
    * demo gifs
    
    * talk with github codespaces
    
    * talk with github codespaces
    
    * gitignore
    
    * linted
    
    * added version
    
    * link fix
    
    * added local llm tag
    
    * crag
    
    * link fix
    
    * lint
    
    * llm tags
    
    * non-clickable badge
    
    * non-clickable badge
    
    * fix
    
    * tutorial llm tags
    
    * added instructions and fix
    
    * colab fix
    
    * fix
    
    * formatted
    
    * hybrid search and rag colab
    
    * colab format
    
    * python test
    
    * node test
    
    * python test
    
    * blog link update
    
    * rag mlx
    
    * myntra search engine app
    
    * link fix
    
    * CrewAI Example
    
    * lint
    
    * node test
    
    * node test
    
    * node test
    
    * added readme
    
    * support for Gemini Pro
    
    * fix
    
    * chunking techniques
    
    * lint
    
    * Locally RAG from Scratch
    
    * lint
    
    * llama3 added
    
    * link finx
    
    * sdk manual cli chatbot phidata
    
    * sdk manual cli chatbot phidata

--- applications/Chatbot_with_Parler_TTS/main.py
+++ applications/Chatbot_with_Parler_TTS/main.py
@@ -1,7 +1,7 @@
-
 import gradio as gr
 from rag_lance import get_rag_output
 from tts_module import text_to_speech
+
 
 def process_question(question, include_audio):
     generated_text = get_rag_output(question)
@@ -11,19 +11,27 @@ def process_question(question, include_audio):
     else:
         return generated_text, None  # Return None for the audio part
 
+
 iface = gr.Interface(
     fn=process_question,
     inputs=[
         gr.Textbox(lines=2, placeholder="Enter a question..."),
-        gr.Checkbox(label="Include audio", value=True)  # Default to True, can be unchecked by user
+        gr.Checkbox(
+            label="Include audio", value=True
+        ),  # Default to True, can be unchecked by user
     ],
     outputs=[
         gr.Textbox(label="Generated Text"),
-        gr.Audio(label="Generated Audio", type="filepath")  # No optional keyword
+        gr.Audio(label="Generated Audio", type="filepath"),  # No optional keyword
     ],
     title="Advance RAG chatbot with TTS support",
     description="Ask a question and get a text response along with its audio representation. Optionally, include the audio response.",
-    examples=[["What is net profit of Airbnb ?"], ["What are the specific factors contributing to Airbnb's increased operational expenses in the last fiscal year"]]
+    examples=[
+        ["What is net profit of Airbnb ?"],
+        [
+            "What are the specific factors contributing to Airbnb's increased operational expenses in the last fiscal year"
+        ],
+    ],
 )
 
 if __name__ == "__main__":
