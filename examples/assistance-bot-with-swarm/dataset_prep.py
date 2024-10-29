5a80f866f203ff3461bb3b4bd5a16a27023b067f
PrashantDixit-dev <prasantdixit987@gmail.com>
2024-10-29 22:10:22 +0530

    data loader

--- examples/assistance-bot-with-swarm/dataset_prep.py
+++ examples/assistance-bot-with-swarm/dataset_prep.py
@@ -1,31 +1,18 @@
-import json
-import os
-
 import pandas as pd
-import lancedb
-
-EMBEDDING_MODEL = "text-embedding-ada-002"
-
-article_list = os.listdir("data")
-
-articles = []
-
-for x in article_list:
-    article_path = "data/" + x
-
-    # Opening JSON file
-    f = open(article_path)
-    data = json.load(f)
-    articles.append(data)
-
-    # Closing file
-    f.close()
-
-# ingest data inside table
 import lancedb
 from lancedb.pydantic import LanceModel, Vector
 from lancedb.embeddings import get_registry
 
+from datasets import load_dataset
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+
+# load dataset
+dataset_name = 'Prasant/openai-dataset'
+dataset = load_dataset(dataset_name)
+article_df = pd.DataFrame(dataset['train'])
+
+# ingest data inside table
 db = lancedb.connect("/tmp/db")
 func = get_registry().get("openai").create(name=EMBEDDING_MODEL)
 
@@ -39,7 +26,6 @@ class Article(LanceModel):
 
 table = db.create_table("help-center", schema=Article, mode="overwrite")
 
-article_df = pd.DataFrame(articles)
 table.add(article_df)
 
 print(len(table))
